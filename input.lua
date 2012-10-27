69a2b78c4af948e1761793c8c6b8feb0cb19794f
William Adams <william_a_adams@msn.com>
2012-10-26 18:01:52 -0700

    Finishing off constants-

--- input.lua
+++ input.lua
@@ -1,6 +1,6 @@
 
 local ffi = require "ffi"
-
+require "include/headers"
 
 --[[
  * Copyright (c) 1999-2002 Vojtech Pavlik
@@ -134,41 +134,41 @@ EVIOCGRAB		_IOW('E', 0x90, int)			--[[ Grab/Release device --]]
  * Device properties and quirks
  --]]
 
-INPUT_PROP_POINTER		= 0x00	--[[ needs a pointer --]]
-INPUT_PROP_DIRECT		= 0x01	--[[ direct input devices --]]
-INPUT_PROP_BUTTONPAD		= 0x02	--[[ has button(s) under pad --]]
-INPUT_PROP_SEMI_MT		= 0x03	--[[ touch rectangle only --]]
+INPUT_PROP_POINTER		= 0x00;	--[[ needs a pointer --]]
+INPUT_PROP_DIRECT		= 0x01;	--[[ direct input devices --]]
+INPUT_PROP_BUTTONPAD	= 0x02;	--[[ has button(s) under pad --]]
+INPUT_PROP_SEMI_MT		= 0x03;	--[[ touch rectangle only --]]
 
-INPUT_PROP_MAX			= 0x1f
-INPUT_PROP_CNT			= (INPUT_PROP_MAX + 1)
+INPUT_PROP_MAX			= 0x1f;
+INPUT_PROP_CNT			= (INPUT_PROP_MAX + 1);
 
 --[[
  * Event types
  --]]
 
-EV_SYN			= 0x00
-EV_KEY			= 0x01
-EV_REL			= 0x02
-EV_ABS			= 0x03
-EV_MSC			= 0x04
-EV_SW			= 0x05
-EV_LED			= 0x11
-EV_SND			= 0x12
-EV_REP			= 0x14
-EV_FF			= 0x15
-EV_PWR			= 0x16
-EV_FF_STATUS		= 0x17
-EV_MAX			= 0x1f
-EV_CNT			= (EV_MAX+1)
+EV_SYN			= 0x00;
+EV_KEY			= 0x01;
+EV_REL			= 0x02;
+EV_ABS			= 0x03;
+EV_MSC			= 0x04;
+EV_SW			= 0x05;
+EV_LED			= 0x11;
+EV_SND			= 0x12;
+EV_REP			= 0x14;
+EV_FF			= 0x15;
+EV_PWR			= 0x16;
+EV_FF_STATUS	= 0x17;
+EV_MAX			= 0x1f;
+EV_CNT			= (EV_MAX+1);
 
 --[[
  * Synchronization events.
  --]]
 
-SYN_REPORT		= 0
-SYN_CONFIG		= 1
-SYN_MT_REPORT		= 2
-SYN_DROPPED		= 3
+SYN_REPORT		= 0;
+SYN_CONFIG		= 1;
+SYN_MT_REPORT	= 2;
+SYN_DROPPED		= 3;
 
 --[[
  * Keys and buttons
@@ -181,7 +181,7 @@ SYN_DROPPED		= 3
  * SC - System Control
  --]]
 
-KEY_RESERVED		= 0;
+KEY_RESERVED	= 0;
 KEY_ESC			= 1;
 KEY_1			= 2;
 KEY_2			= 3;
@@ -195,7 +195,7 @@ KEY_9			= 10;
 KEY_0			= 11;
 KEY_MINUS		= 12;
 KEY_EQUAL		= 13;
-KEY_BACKSPACE		= 14;
+KEY_BACKSPACE	= 14;
 KEY_TAB			= 15;
 KEY_Q			= 16;
 KEY_W			= 17;
@@ -207,10 +207,10 @@ KEY_U			= 22;
 KEY_I			= 23;
 KEY_O			= 24;
 KEY_P			= 25;
-KEY_LEFTBRACE		= 26;
-KEY_RIGHTBRACE		= 27;
+KEY_LEFTBRACE	= 26;
+KEY_RIGHTBRACE	= 27;
 KEY_ENTER		= 28;
-KEY_LEFTCTRL		= 29;
+KEY_LEFTCTRL	= 29;
 KEY_A			= 30;
 KEY_S			= 31;
 KEY_D			= 32;
@@ -220,11 +220,11 @@ KEY_H			= 35;
 KEY_J			= 36;
 KEY_K			= 37;
 KEY_L			= 38;
-KEY_SEMICOLON		= 39;
-KEY_APOSTROPHE		= 40;
+KEY_SEMICOLON	= 39;
+KEY_APOSTROPHE	= 40;
 KEY_GRAVE		= 41;
-KEY_LEFTSHIFT		= 42;
-KEY_BACKSLASH		= 43;
+KEY_LEFTSHIFT	= 42;
+KEY_BACKSLASH	= 43;
 KEY_Z			= 44;
 KEY_X			= 45;
 KEY_C			= 46;
@@ -235,11 +235,11 @@ KEY_M			= 50;
 KEY_COMMA		= 51;
 KEY_DOT			= 52;
 KEY_SLASH		= 53;
-KEY_RIGHTSHIFT		= 54;
-KEY_KPASTERISK		= 55;
+KEY_RIGHTSHIFT	= 54;
+KEY_KPASTERISK	= 55;
 KEY_LEFTALT		= 56;
 KEY_SPACE		= 57;
-KEY_CAPSLOCK		= 58;
+KEY_CAPSLOCK	= 58;
 KEY_F1			= 59;
 KEY_F2			= 60;
 KEY_F3			= 61;
@@ -251,7 +251,7 @@ KEY_F8			= 66;
 KEY_F9			= 67;
 KEY_F10			= 68;
 KEY_NUMLOCK		= 69;
-KEY_SCROLLLOCK		= 70;
+KEY_SCROLLLOCK	= 70;
 KEY_KP7			= 71;
 KEY_KP8			= 72;
 KEY_KP9			= 73;
@@ -271,18 +271,18 @@ KEY_102ND		= 86;
 KEY_F11			= 87;
 KEY_F12			= 88;
 KEY_RO			= 89;
-KEY_KATAKANA		= 90;
-KEY_HIRAGANA		= 91;
+KEY_KATAKANA	= 90;
+KEY_HIRAGANA	= 91;
 KEY_HENKAN		= 92;
 KEY_KATAKANAHIRAGANA	= 93;
-KEY_MUHENKAN		= 94;
-KEY_KPJPCOMMA		= 95;
+KEY_MUHENKAN	= 94;
+KEY_KPJPCOMMA	= 95;
 KEY_KPENTER		= 96;
-KEY_RIGHTCTRL		= 97;
+KEY_RIGHTCTRL	= 97;
 KEY_KPSLASH		= 98;
 KEY_SYSRQ		= 99;
-KEY_RIGHTALT		= 100;
-KEY_LINEFEED		= 101;
+KEY_RIGHTALT	= 100;
+KEY_LINEFEED	= 101;
 KEY_HOME		= 102;
 KEY_UP			= 103;
 KEY_PAGEUP		= 104;
@@ -290,16 +290,16 @@ KEY_LEFT		= 105;
 KEY_RIGHT		= 106;
 KEY_END			= 107;
 KEY_DOWN		= 108;
-KEY_PAGEDOWN		= 109;
+KEY_PAGEDOWN	= 109;
 KEY_INSERT		= 110;
 KEY_DELETE		= 111;
 KEY_MACRO		= 112;
 KEY_MUTE		= 113;
-KEY_VOLUMEDOWN		= 114;
-KEY_VOLUMEUP		= 115;
+KEY_VOLUMEDOWN	= 114;
+KEY_VOLUMEUP	= 115;
 KEY_POWER		= 116;	--[[ SC System Power Down --]]
 KEY_KPEQUAL		= 117;
-KEY_KPPLUSMINUS		= 118;
+KEY_KPPLUSMINUS	= 118;
 KEY_PAUSE		= 119;
 KEY_SCALE		= 120;	--[[ AL Compiz Scale (Expose) --]]
 
@@ -308,8 +308,8 @@ KEY_HANGEUL		= 122;
 KEY_HANGUEL		= KEY_HANGEUL;
 KEY_HANJA		= 123;
 KEY_YEN			= 124;
-KEY_LEFTMETA		= 125;
-KEY_RIGHTMETA		= 126;
+KEY_LEFTMETA	= 125;
+KEY_RIGHTMETA	= 126;
 KEY_COMPOSE		= 127;
 
 KEY_STOP		= 128;	--[[ AC Stop --]]
@@ -329,28 +329,28 @@ KEY_SETUP		= 141;
 KEY_SLEEP		= 142;	--[[ SC System Sleep --]]
 KEY_WAKEUP		= 143;	--[[ System Wake Up --]]
 KEY_FILE		= 144;	--[[ AL Local Machine Browser --]]
-KEY_SENDFILE		= 145;
-KEY_DELETEFILE		= 146;
+KEY_SENDFILE	= 145;
+KEY_DELETEFILE	= 146;
 KEY_XFER		= 147;
 KEY_PROG1		= 148;
 KEY_PROG2		= 149;
 KEY_WWW			= 150;	--[[ AL Internet Browser --]]
 KEY_MSDOS		= 151;
 KEY_COFFEE		= 152;	--[[ AL Terminal Lock/Screensaver --]]
-KEY_SCREENLOCK		= KEY_COFFEE;
-KEY_DIRECTION		= 153;
+KEY_SCREENLOCK	= KEY_COFFEE;
+KEY_DIRECTION	= 153;
 KEY_CYCLEWINDOWS	= 154;
 KEY_MAIL		= 155;
-KEY_BOOKMARKS		= 156;	--[[ AC Bookmarks --]]
-KEY_COMPUTER		= 157;
+KEY_BOOKMARKS	= 156;	--[[ AC Bookmarks --]]
+KEY_COMPUTER	= 157;
 KEY_BACK		= 158;	--[[ AC Back --]]
 KEY_FORWARD		= 159;	--[[ AC Forward --]]
 KEY_CLOSECD		= 160;
 KEY_EJECTCD		= 161;
-KEY_EJECTCLOSECD	= 162;
-KEY_NEXTSONG		= 163;
-KEY_PLAYPAUSE		= 164;
-KEY_PREVIOUSSONG	= 165;
+KEY_EJECTCLOSECD= 162;
+KEY_NEXTSONG	= 163;
+KEY_PLAYPAUSE	= 164;
+KEY_PREVIOUSSONG= 165;
 KEY_STOPCD		= 166;
 KEY_RECORD		= 167;
 KEY_REWIND		= 168;
@@ -443,166 +443,166 @@ KEY_MICMUTE		= 248;	--[[ Mute / unmute the microphone --]]
 
 --[[ Code 255 is reserved for special needs of AT keyboard driver --]]
 
-BTN_MISC		0x100;
-BTN_0			0x100;
-BTN_1			0x101;
-BTN_2			0x102;
-BTN_3			0x103;
-BTN_4			0x104;
-BTN_5			0x105;
-BTN_6			0x106;
-BTN_7			0x107;
-BTN_8			0x108;
-BTN_9			0x109;
+BTN_MISC		= 0x100;
+BTN_0			= 0x100;
+BTN_1			= 0x101;
+BTN_2			= 0x102;
+BTN_3			= 0x103;
+BTN_4			= 0x104;
+BTN_5			= 0x105;
+BTN_6			= 0x106;
+BTN_7			= 0x107;
+BTN_8			= 0x108;
+BTN_9			= 0x109;
 
-BTN_MOUSE		0x110;
-BTN_LEFT		0x110;
-BTN_RIGHT		0x111;
-BTN_MIDDLE		0x112;
-BTN_SIDE		0x113;
-BTN_EXTRA		0x114;
-BTN_FORWARD		0x115;
-BTN_BACK		0x116;
-BTN_TASK		0x117;
+BTN_MOUSE		= 0x110;
+BTN_LEFT		= 0x110;
+BTN_RIGHT		= 0x111;
+BTN_MIDDLE		= 0x112;
+BTN_SIDE		= 0x113;
+BTN_EXTRA		= 0x114;
+BTN_FORWARD		= 0x115;
+BTN_BACK		= 0x116;
+BTN_TASK		= 0x117;
 
-BTN_JOYSTICK		0x120;
-BTN_TRIGGER		0x120;
-BTN_THUMB		0x121;
-BTN_THUMB2		0x122;
-BTN_TOP			0x123;
-BTN_TOP2		0x124;
-BTN_PINKIE		0x125;
-BTN_BASE		0x126;
-BTN_BASE2		0x127;
-BTN_BASE3		0x128;
-BTN_BASE4		0x129;
-BTN_BASE5		0x12a;
-BTN_BASE6		0x12b;
-BTN_DEAD		0x12f;
+BTN_JOYSTICK		= 0x120;
+BTN_TRIGGER		= 0x120;
+BTN_THUMB		= 0x121;
+BTN_THUMB2		= 0x122;
+BTN_TOP			= 0x123;
+BTN_TOP2		= 0x124;
+BTN_PINKIE		= 0x125;
+BTN_BASE		= 0x126;
+BTN_BASE2		= 0x127;
+BTN_BASE3		= 0x128;
+BTN_BASE4		= 0x129;
+BTN_BASE5		= 0x12a;
+BTN_BASE6		= 0x12b;
+BTN_DEAD		= 0x12f;
 
-BTN_GAMEPAD		0x130;
-BTN_A			0x130;
-BTN_B			0x131;
-BTN_C			0x132;
-BTN_X			0x133;
-BTN_Y			0x134;
-BTN_Z			0x135;
-BTN_TL			0x136;
-BTN_TR			0x137;
-BTN_TL2			0x138;
-BTN_TR2			0x139;
-BTN_SELECT		0x13a;
-BTN_START		0x13b;
-BTN_MODE		0x13c;
-BTN_THUMBL		0x13d;
-BTN_THUMBR		0x13e;
+BTN_GAMEPAD		= 0x130;
+BTN_A			= 0x130;
+BTN_B			= 0x131;
+BTN_C			= 0x132;
+BTN_X			= 0x133;
+BTN_Y			= 0x134;
+BTN_Z			= 0x135;
+BTN_TL			= 0x136;
+BTN_TR			= 0x137;
+BTN_TL2			= 0x138;
+BTN_TR2			= 0x139;
+BTN_SELECT		= 0x13a;
+BTN_START		= 0x13b;
+BTN_MODE		= 0x13c;
+BTN_THUMBL		= 0x13d;
+BTN_THUMBR		= 0x13e;
 
-BTN_DIGI		0x140;
-BTN_TOOL_PEN		0x140;
-BTN_TOOL_RUBBER		0x141;
-BTN_TOOL_BRUSH		0x142;
-BTN_TOOL_PENCIL		0x143;
-BTN_TOOL_AIRBRUSH	0x144;
-BTN_TOOL_FINGER		0x145;
-BTN_TOOL_MOUSE		0x146;
-BTN_TOOL_LENS		0x147;
-BTN_TOOL_QUINTTAP	0x148;	--[[ Five fingers on trackpad --]]
-BTN_TOUCH		0x14a;
-BTN_STYLUS		0x14b;
-BTN_STYLUS2		0x14c;
-BTN_TOOL_DOUBLETAP	0x14d;
-BTN_TOOL_TRIPLETAP	0x14e;
-BTN_TOOL_QUADTAP	0x14f;	--[[ Four fingers on trackpad --]]
+BTN_DIGI		= 0x140;
+BTN_TOOL_PEN		= 0x140;
+BTN_TOOL_RUBBER		= 0x141;
+BTN_TOOL_BRUSH		= 0x142;
+BTN_TOOL_PENCIL		= 0x143;
+BTN_TOOL_AIRBRUSH	= 0x144;
+BTN_TOOL_FINGER		= 0x145;
+BTN_TOOL_MOUSE		= 0x146;
+BTN_TOOL_LENS		= 0x147;
+BTN_TOOL_QUINTTAP	= 0x148;	--[[ Five fingers on trackpad --]]
+BTN_TOUCH		= 0x14a;
+BTN_STYLUS		= 0x14b;
+BTN_STYLUS2		= 0x14c;
+BTN_TOOL_DOUBLETAP	= 0x14d;
+BTN_TOOL_TRIPLETAP	= 0x14e;
+BTN_TOOL_QUADTAP	= 0x14f;	--[[ Four fingers on trackpad --]]
 
-BTN_WHEEL		0x150;
-BTN_GEAR_DOWN		0x150;
-BTN_GEAR_UP		0x151;
+BTN_WHEEL		= 0x150;
+BTN_GEAR_DOWN		= 0x150;
+BTN_GEAR_UP		= 0x151;
 
-KEY_OK			0x160;
-KEY_SELECT		0x161;
-KEY_GOTO		0x162;
-KEY_CLEAR		0x163;
-KEY_POWER2		0x164;
-KEY_OPTION		0x165;
-KEY_INFO		0x166;	--[[ AL OEM Features/Tips/Tutorial --]]
-KEY_TIME		0x167;
-KEY_VENDOR		0x168;
-KEY_ARCHIVE		0x169;
-KEY_PROGRAM		0x16a;	--[[ Media Select Program Guide --]]
-KEY_CHANNEL		0x16b;
-KEY_FAVORITES		0x16c;
-KEY_EPG			0x16d;
-KEY_PVR			0x16e;	--[[ Media Select Home --]]
-KEY_MHP			0x16f;
-KEY_LANGUAGE		0x170;
-KEY_TITLE		0x171;
-KEY_SUBTITLE		0x172;
-KEY_ANGLE		0x173;
-KEY_ZOOM		0x174;
-KEY_MODE		0x175;
-KEY_KEYBOARD		0x176;
-KEY_SCREEN		0x177;
-KEY_PC			0x178;	--[[ Media Select Computer --]]
-KEY_TV			0x179;	--[[ Media Select TV --]]
-KEY_TV2			0x17a;	--[[ Media Select Cable --]]
-KEY_VCR			0x17b;	--[[ Media Select VCR --]]
-KEY_VCR2		0x17c;	--[[ VCR Plus --]]
-KEY_SAT			0x17d;	--[[ Media Select Satellite --]]
-KEY_SAT2		0x17e;
-KEY_CD			0x17f;	--[[ Media Select CD --]]
-KEY_TAPE		0x180;	--[[ Media Select Tape --]]
-KEY_RADIO		0x181;
-KEY_TUNER		0x182;	--[[ Media Select Tuner --]]
-KEY_PLAYER		0x183;
-KEY_TEXT		0x184;
-KEY_DVD			0x185;	--[[ Media Select DVD --]]
-KEY_AUX			0x186;
-KEY_MP3			0x187;
-KEY_AUDIO		0x188;	--[[ AL Audio Browser --]]
-KEY_VIDEO		0x189;	--[[ AL Movie Browser --]]
-KEY_DIRECTORY		0x18a;
-KEY_LIST		0x18b;
-KEY_MEMO		0x18c;	--[[ Media Select Messages --]]
-KEY_CALENDAR		0x18d;
-KEY_RED			0x18e;
-KEY_GREEN		0x18f;
-KEY_YELLOW		0x190;
-KEY_BLUE		0x191;
-KEY_CHANNELUP		0x192;	--[[ Channel Increment --]]
-KEY_CHANNELDOWN		0x193;	--[[ Channel Decrement --]]
-KEY_FIRST		0x194;
-KEY_LAST		0x195;	--[[ Recall Last --]]
-KEY_AB			0x196;
-KEY_NEXT		0x197;
-KEY_RESTART		0x198;
-KEY_SLOW		0x199;
-KEY_SHUFFLE		0x19a;
-KEY_BREAK		0x19b;
-KEY_PREVIOUS		0x19c;
-KEY_DIGITS		0x19d;
-KEY_TEEN		0x19e;
-KEY_TWEN		0x19f;
-KEY_VIDEOPHONE		0x1a0;	--[[ Media Select Video Phone --]]
-KEY_GAMES		0x1a1;	--[[ Media Select Games --]]
-KEY_ZOOMIN		0x1a2;	--[[ AC Zoom In --]]
-KEY_ZOOMOUT		0x1a3;	--[[ AC Zoom Out --]]
-KEY_ZOOMRESET		0x1a4;	--[[ AC Zoom --]]
-KEY_WORDPROCESSOR	0x1a5;	--[[ AL Word Processor --]]
-KEY_EDITOR		0x1a6;	--[[ AL Text Editor --]]
-KEY_SPREADSHEET		0x1a7;	--[[ AL Spreadsheet --]]
-KEY_GRAPHICSEDITOR	0x1a8;	--[[ AL Graphics Editor --]]
-KEY_PRESENTATION	0x1a9;	--[[ AL Presentation App --]]
-KEY_DATABASE		0x1aa;	--[[ AL Database App --]]
-KEY_NEWS		0x1ab;	--[[ AL Newsreader --]]
-KEY_VOICEMAIL		0x1ac;	--[[ AL Voicemail --]]
-KEY_ADDRESSBOOK		0x1ad;	--[[ AL Contacts/Address Book --]]
-KEY_MESSENGER		0x1ae;	--[[ AL Instant Messaging --]]
-KEY_DISPLAYTOGGLE	0x1af;	--[[ Turn display (LCD) on and off --]]
-KEY_SPELLCHECK		0x1b0;  --[[ AL Spell Check --]]
-KEY_LOGOFF		0x1b1;   --[[ AL Logoff --]]
+KEY_OK			= 0x160;
+KEY_SELECT		= 0x161;
+KEY_GOTO		= 0x162;
+KEY_CLEAR		= 0x163;
+KEY_POWER2		= 0x164;
+KEY_OPTION		= 0x165;
+KEY_INFO		= 0x166;	--[[ AL OEM Features/Tips/Tutorial --]]
+KEY_TIME		= 0x167;
+KEY_VENDOR		= 0x168;
+KEY_ARCHIVE		= 0x169;
+KEY_PROGRAM		= 0x16a;	--[[ Media Select Program Guide --]]
+KEY_CHANNEL		= 0x16b;
+KEY_FAVORITES		= 0x16c;
+KEY_EPG			= 0x16d;
+KEY_PVR			= 0x16e;	--[[ Media Select Home --]]
+KEY_MHP			= 0x16f;
+KEY_LANGUAGE		= 0x170;
+KEY_TITLE		= 0x171;
+KEY_SUBTITLE		= 0x172;
+KEY_ANGLE		= 0x173;
+KEY_ZOOM		= 0x174;
+KEY_MODE		= 0x175;
+KEY_KEYBOARD		= 0x176;
+KEY_SCREEN		= 0x177;
+KEY_PC			= 0x178;	--[[ Media Select Computer --]]
+KEY_TV			= 0x179;	--[[ Media Select TV --]]
+KEY_TV2			= 0x17a;	--[[ Media Select Cable --]]
+KEY_VCR			= 0x17b;	--[[ Media Select VCR --]]
+KEY_VCR2		= 0x17c;	--[[ VCR Plus --]]
+KEY_SAT			= 0x17d;	--[[ Media Select Satellite --]]
+KEY_SAT2		= 0x17e;
+KEY_CD			= 0x17f;	--[[ Media Select CD --]]
+KEY_TAPE		= 0x180;	--[[ Media Select Tape --]]
+KEY_RADIO		= 0x181;
+KEY_TUNER		= 0x182;	--[[ Media Select Tuner --]]
+KEY_PLAYER		= 0x183;
+KEY_TEXT		= 0x184;
+KEY_DVD			= 0x185;	--[[ Media Select DVD --]]
+KEY_AUX			= 0x186;
+KEY_MP3			= 0x187;
+KEY_AUDIO		= 0x188;	--[[ AL Audio Browser --]]
+KEY_VIDEO		= 0x189;	--[[ AL Movie Browser --]]
+KEY_DIRECTORY		= 0x18a;
+KEY_LIST		= 0x18b;
+KEY_MEMO		= 0x18c;	--[[ Media Select Messages --]]
+KEY_CALENDAR		= 0x18d;
+KEY_RED			= 0x18e;
+KEY_GREEN		= 0x18f;
+KEY_YELLOW		= 0x190;
+KEY_BLUE		= 0x191;
+KEY_CHANNELUP		= 0x192;	--[[ Channel Increment --]]
+KEY_CHANNELDOWN		= 0x193;	--[[ Channel Decrement --]]
+KEY_FIRST		= 0x194;
+KEY_LAST		= 0x195;	--[[ Recall Last --]]
+KEY_AB			= 0x196;
+KEY_NEXT		= 0x197;
+KEY_RESTART		= 0x198;
+KEY_SLOW		= 0x199;
+KEY_SHUFFLE		= 0x19a;
+KEY_BREAK		= 0x19b;
+KEY_PREVIOUS		= 0x19c;
+KEY_DIGITS		= 0x19d;
+KEY_TEEN		= 0x19e;
+KEY_TWEN			= 0x19f;
+KEY_VIDEOPHONE		= 0x1a0;	--[[ Media Select Video Phone --]]
+KEY_GAMES			= 0x1a1;	--[[ Media Select Games --]]
+KEY_ZOOMIN			= 0x1a2;	--[[ AC Zoom In --]]
+KEY_ZOOMOUT			= 0x1a3;	--[[ AC Zoom Out --]]
+KEY_ZOOMRESET		= 0x1a4;	--[[ AC Zoom --]]
+KEY_WORDPROCESSOR	= 0x1a5;	--[[ AL Word Processor --]]
+KEY_EDITOR			= 0x1a6;	--[[ AL Text Editor --]]
+KEY_SPREADSHEET		= 0x1a7;	--[[ AL Spreadsheet --]]
+KEY_GRAPHICSEDITOR	= 0x1a8;	--[[ AL Graphics Editor --]]
+KEY_PRESENTATION	= 0x1a9;	--[[ AL Presentation App --]]
+KEY_DATABASE		= 0x1aa;	--[[ AL Database App --]]
+KEY_NEWS			= 0x1ab;	--[[ AL Newsreader --]]
+KEY_VOICEMAIL		= 0x1ac;	--[[ AL Voicemail --]]
+KEY_ADDRESSBOOK		= 0x1ad;	--[[ AL Contacts/Address Book --]]
+KEY_MESSENGER		= 0x1ae;	--[[ AL Instant Messaging --]]
+KEY_DISPLAYTOGGLE	= 0x1af;	--[[ Turn display (LCD) on and off --]]
+KEY_SPELLCHECK		= 0x1b0;  --[[ AL Spell Check --]]
+KEY_LOGOFF			= 0x1b1;   --[[ AL Logoff --]]
 
-KEY_DOLLAR		= 0x1b2;
-KEY_EURO		= 0x1b3;
+KEY_DOLLAR			= 0x1b2;
+KEY_EURO			= 0x1b3;
 
 KEY_FRAMEBACK		= 0x1b4;	--[[ Consumer - transport controls --]]
 KEY_FRAMEFORWARD	= 0x1b5;
@@ -610,10 +610,10 @@ KEY_CONTEXT_MENU	= 0x1b6;	--[[ GenDesc - system context menu --]]
 KEY_MEDIA_REPEAT	= 0x1b7;	--[[ Consumer - transport control --]]
 KEY_10CHANNELSUP	= 0x1b8;	--[[ 10 channels up (10+) --]]
 KEY_10CHANNELSDOWN	= 0x1b9;	--[[ 10 channels down (10-) --]]
-KEY_IMAGES		= 0x1ba;	--[[ AL Image Browser --]]
+KEY_IMAGES			= 0x1ba;	--[[ AL Image Browser --]]
 
-KEY_DEL_EOL		= 0x1c0;
-KEY_DEL_EOS		= 0x1c1;
+KEY_DEL_EOL			= 0x1c0;
+KEY_DEL_EOS			= 0x1c1;
 KEY_INS_LINE		= 0x1c2;
 KEY_DEL_LINE		= 0x1c3;
 
@@ -720,9 +720,9 @@ BTN_TRIGGER_HAPPY39		= 0x2e6;
 BTN_TRIGGER_HAPPY40		= 0x2e7;
 
 --[[ We avoid low common keys in module aliases so they don't get huge. --]]
-KEY_MIN_INTERESTING	= KEY_MUTE;
-KEY_MAX			= 0x2ff;
-KEY_CNT			= (KEY_MAX+1);
+KEY_MIN_INTERESTING		= KEY_MUTE;
+KEY_MAX					= 0x2ff;
+KEY_CNT					= (KEY_MAX+1);
 
 --[[
  * Relative axes
@@ -751,7 +751,7 @@ ABS_Z			= 0x02;
 ABS_RX			= 0x03;
 ABS_RY			= 0x04;
 ABS_RZ			= 0x05;
-ABS_THROTTLE		= 0x06;
+ABS_THROTTLE	= 0x06;
 ABS_RUDDER		= 0x07;
 ABS_WHEEL		= 0x08;
 ABS_GAS			= 0x09;
@@ -764,11 +764,11 @@ ABS_HAT2X		= 0x14;
 ABS_HAT2Y		= 0x15;
 ABS_HAT3X		= 0x16;
 ABS_HAT3Y		= 0x17;
-ABS_PRESSURE		= 0x18;
-ABS_DISTANCE		= 0x19;
+ABS_PRESSURE	= 0x18;
+ABS_DISTANCE	= 0x19;
 ABS_TILT_X		= 0x1a;
 ABS_TILT_Y		= 0x1b;
-ABS_TOOL_WIDTH		= 0x1c;
+ABS_TOOL_WIDTH	= 0x1c;
 
 ABS_VOLUME		= 0x20;
 
@@ -796,31 +796,31 @@ ABS_CNT			= (ABS_MAX+1);
  * Switch events
  --]]
 
-SW_LID			= 0x00;  --[[ set = lid shut --]]
-SW_TABLET_MODE		= 0x01;  --[[ set = tablet mode --]]
-SW_HEADPHONE_INSERT	= 0x02;  --[[ set = inserted --]]
-SW_RFKILL_ALL		= 0x03;  --[[ rfkill master switch, type "any"
+SW_LID					= 0x00;  --[[ set = lid shut --]]
+SW_TABLET_MODE			= 0x01;  --[[ set = tablet mode --]]
+SW_HEADPHONE_INSERT		= 0x02;  --[[ set = inserted --]]
+SW_RFKILL_ALL			= 0x03;  --[[ rfkill master switch, type "any"
 					 set = radio enabled --]]
-SW_RADIO		= SW_RFKILL_ALL;	--[[ deprecated --]]
+SW_RADIO				= SW_RFKILL_ALL;	--[[ deprecated --]]
 SW_MICROPHONE_INSERT	= 0x04;  --[[ set = inserted --]]
-SW_DOCK			= 0x05;  --[[ set = plugged into dock --]]
-SW_LINEOUT_INSERT	= 0x06;  --[[ set = inserted --]]
+SW_DOCK					= 0x05;  --[[ set = plugged into dock --]]
+SW_LINEOUT_INSERT		= 0x06;  --[[ set = inserted --]]
 SW_JACK_PHYSICAL_INSERT = 0x07;  --[[ set = mechanical switch set --]]
-SW_VIDEOOUT_INSERT	= 0x08;  --[[ set = inserted --]]
+SW_VIDEOOUT_INSERT		= 0x08;  --[[ set = inserted --]]
 SW_CAMERA_LENS_COVER	= 0x09;  --[[ set = lens covered --]]
-SW_KEYPAD_SLIDE		= 0x0a;  --[[ set = keypad slide out --]]
-SW_FRONT_PROXIMITY	= 0x0b;  --[[ set = front proximity sensor active --]]
-SW_ROTATE_LOCK		= 0x0c;  --[[ set = rotate locked/disabled --]]
-SW_LINEIN_INSERT	= 0x0d;  --[[ set = inserted --]]
-SW_MAX			= 0x0f;
-SW_CNT			= (SW_MAX+1);
+SW_KEYPAD_SLIDE			= 0x0a;  --[[ set = keypad slide out --]]
+SW_FRONT_PROXIMITY		= 0x0b;  --[[ set = front proximity sensor active --]]
+SW_ROTATE_LOCK			= 0x0c;  --[[ set = rotate locked/disabled --]]
+SW_LINEIN_INSERT		= 0x0d;  --[[ set = inserted --]]
+SW_MAX					= 0x0f;
+SW_CNT					= (SW_MAX+1);
 
 --[[
  * Misc events
  --]]
 
 MSC_SERIAL		= 0x00;
-MSC_PULSELED		= 0x01;
+MSC_PULSELED	= 0x01;
 MSC_GESTURE		= 0x02;
 MSC_RAW			= 0x03;
 MSC_SCAN		= 0x04;
@@ -841,7 +841,7 @@ LED_SUSPEND		= 0x06;
 LED_MUTE		= 0x07;
 LED_MISC		= 0x08;
 LED_MAIL		= 0x09;
-LED_CHARGING		= 0x0a;
+LED_CHARGING	= 0x0a;
 LED_MAX			= 0x0f;
 LED_CNT			= (LED_MAX+1);
 
@@ -877,14 +877,14 @@ BUS_PCI			= 0x01;
 BUS_ISAPNP		= 0x02;
 BUS_USB			= 0x03;
 BUS_HIL			= 0x04;
-BUS_BLUETOOTH		= 0x05;
+BUS_BLUETOOTH	= 0x05;
 BUS_VIRTUAL		= 0x06;
 
 BUS_ISA			= 0x10;
 BUS_I8042		= 0x11;
 BUS_XTKBD		= 0x12;
 BUS_RS232		= 0x13;
-BUS_GAMEPORT		= 0x14;
+BUS_GAMEPORT	= 0x14;
 BUS_PARPORT		= 0x15;
 BUS_AMIGA		= 0x16;
 BUS_ADB			= 0x17;
@@ -897,7 +897,7 @@ BUS_SPI			= 0x1C;
 --[[
  * MT_TOOL types
  --]]
-MT_TOOL_FINGER		= 0;
+MT_TOOL_FINGER	= 0;
 MT_TOOL_PEN		= 1;
 MT_TOOL_MAX		= 1;
 
@@ -1095,7 +1095,7 @@ struct ff_effect {
 		struct ff_constant_effect constant;
 		struct ff_ramp_effect ramp;
 		struct ff_periodic_effect periodic;
-		struct ff_condition_effect condition[2]; --[[ One for each axis --]]
+		struct ff_condition_effect condition[2]; // One for each axis 
 		struct ff_rumble_effect rumble;
 	} u;
 };
@@ -1105,14 +1105,14 @@ struct ff_effect {
  * Force feedback effect types
  --]]
 
-FF_RUMBLE	0x50;
-FF_PERIODIC	0x51;
-FF_CONSTANT	0x52;
-FF_SPRING	0x53;
-FF_FRICTION	0x54;
-FF_DAMPER	0x55;
-FF_INERTIA	0x56;
-FF_RAMP		0x57;
+FF_RUMBLE	= 0x50;
+FF_PERIODIC	= 0x51;
+FF_CONSTANT	= 0x52;
+FF_SPRING	= 0x53;
+FF_FRICTION	= 0x54;
+FF_DAMPER	= 0x55;
+FF_INERTIA	= 0x56;
+FF_RAMP		= 0x57;
 
 FF_EFFECT_MIN	= FF_RUMBLE;
 FF_EFFECT_MAX	= FF_RAMP;
@@ -1121,12 +1121,12 @@ FF_EFFECT_MAX	= FF_RAMP;
  * Force feedback periodic effect types
  --]]
 
-FF_SQUARE	0x58;
-FF_TRIANGLE	0x59;
-FF_SINE		0x5a;
-FF_SAW_UP	0x5b;
-FF_SAW_DOWN	0x5c;
-FF_CUSTOM	0x5d;
+FF_SQUARE		= 0x58;
+FF_TRIANGLE		= 0x59;
+FF_SINE			= 0x5a;
+FF_SAW_UP		= 0x5b;
+FF_SAW_DOWN		= 0x5c;
+FF_CUSTOM		= 0x5d;
 
 FF_WAVEFORM_MIN	= FF_SQUARE;
 FF_WAVEFORM_MAX	= FF_CUSTOM;
@@ -1135,10 +1135,10 @@ FF_WAVEFORM_MAX	= FF_CUSTOM;
  * Set ff device properties
  --]]
 
-FF_GAIN		0x60;
-FF_AUTOCENTER	0x61;
+FF_GAIN			= 0x60;
+FF_AUTOCENTER	= 0x61;
 
-FF_MAX		0x7f;
-FF_CNT		= (FF_MAX+1);
+FF_MAX			= 0x7f;
+FF_CNT			= (FF_MAX+1);
 
 
